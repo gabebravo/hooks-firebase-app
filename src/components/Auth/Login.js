184c2bba4f30e7195ca99fb2b11a4441294b9de0
Gabe Bravo <gabecbravo@gmail.com>
2019-07-22 12:05:12 -0400

    Added the login page

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import { withRouter } from 'react-router-dom';
 
-function Login(props) {
-  return <div style={{ fontSize: '40rem' }}>Login</div>;
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+
+class Authenticator extends React.Component {
+  state = {
+    showSignIn: true
+  };
+
+  switchState = showSignIn => {
+    this.setState({ showSignIn });
+  };
+
+  handleChange = value => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { showSignIn } = this.state;
+    const tabIndex = showSignIn ? 0 : 1;
+
+    return (
+      <div>
+        {showSignIn ? <SignIn /> : <SignUp redirect={this.switchState} />}
+        <Tabs
+          centered
+          value={tabIndex}
+          indicatorColor="primary"
+          textColor="primary"
+          onChange={this.handleChange}
+        >
+          <Tab label="Sign In" onClick={() => this.switchState(true)} />
+          <Tab label="Sign Up" onClick={() => this.switchState(false)} />
+        </Tabs>
+      </div>
+    );
+  }
 }
 
-export default Login;
+export default withRouter(Authenticator);
---
+++ src/components/Auth/SignIn.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import LockIcon from '@material-ui/icons/LockOutlined';
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  FormControl,
+  Input,
+  InputLabel,
+  Paper,
+  Typography
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginTop: '10%'
+  },
+  layout: {
+    width: 'auto',
+    display: 'block', // Fix IE11 issue.
+    marginLeft: theme.spacing(3),
+    marginRight: theme.spacing(3),
+    [theme.breakpoints.up(400 + theme.spacing(3 * 2))]: {
+      width: 400,
+      marginLeft: 'auto',
+      marginRight: 'auto'
+    }
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: `${theme.spacing(2)}px ${theme.spacing(3)}px ${theme.spacing(3)}px`
+  },
+  avatar: {
+    margin: theme.spacing.unit,
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: '100%', // Fix IE11 issue.
+    marginTop: theme.spacing.unit
+  },
+  submit: {
+    marginTop: theme.spacing(3)
+  }
+}));
+
+function SignIn() {
+  const classes = useStyles();
+  const [fieldsObj, fieldSetter] = React.useState({
+    username: '',
+    password: ''
+  });
+  const { username, password } = fieldsObj;
+
+  function fieldHandler(evt) {
+    const { name, value } = evt.target;
+    fieldSetter({ ...fieldsObj, [name]: value });
+  }
+
+  function signIn() {
+    console.log('login clicked');
+  }
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockIcon />
+          </Avatar>
+          <Typography>Sign In</Typography>
+          <form className={classes.form}>
+            <FormControl margin="normal" fullWidth required>
+              <InputLabel htmlFor="username">Username</InputLabel>
+              <Input
+                value={username}
+                name="username"
+                autoComplete="off"
+                autoFocus
+                onChange={fieldHandler}
+              />
+            </FormControl>
+            <FormControl margin="normal" fullWidth required>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input
+                value={password}
+                name="password"
+                autoComplete="off"
+                type="password"
+                onChange={fieldHandler}
+              />
+            </FormControl>
+            <Button
+              onClick={() => signIn()}
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Login
+            </Button>
+          </form>
+        </Paper>
+      </main>
+    </div>
+  );
+}
+
+// SignIn.propTypes = {
+//   classes: PropTypes.object.isRequired
+// };
+
+export default SignIn;
