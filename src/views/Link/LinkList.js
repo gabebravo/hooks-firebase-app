dee030f90b52905c41cc737c178de32b889b507e
Gabe Bravo <gabecbravo@gmail.com>
2019-08-13 12:29:00 -0400

    Setting up the reads from the DB for the main page of links

--- src/views/Link/LinkList.js
+++ src/views/Link/LinkList.js
@@ -1,6 +1,27 @@
 import React from 'react';
+import { FirebaseContext } from '../../context';
 
 function LinkList(props) {
+  const { firebase } = React.useContext(FirebaseContext);
+
+  React.useEffect(() => {
+    getLinks();
+  }, []);
+
+  function getLinks() {
+    // onSnapShot can call a callback if its passed as an arg
+    firebase.db.collection('links').onSnapshot(handleSnapshot);
+  }
+
+  // the snapshot arg is a copy of our data >> snapshot.docs is an array
+  function handleSnapshot(snapshot) {
+    const links = snapshot.docs.map(doc => {
+      return { id: doc.id, ...doc.data() };
+    });
+
+    console.log('links:', { links });
+  }
+
   return <div style={{ fontSize: '22rem' }}>LinkList</div>;
 }
 
